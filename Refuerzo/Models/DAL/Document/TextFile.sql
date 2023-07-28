da5e1398314114212ff750f17d295567d1400c62
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-07-27 20:23:53 -0600

    Base de Datos

---
+++ Refuerzo/Models/DAL/Document/TextFile.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE MiBaseDeDatos;
+GO
+
+USE MiBaseDeDatos;
+GO
+
+CREATE TABLE MiTabla (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre NVARCHAR(50),
+    Edad INT,
+    CorreoElectronico NVARCHAR(100)
+);
+GO
+
+INSERT INTO MiTabla (Nombre, Edad, CorreoElectronico)
+VALUES ('Juan', 30, 'juan@example.com');
+
+INSERT INTO MiTabla (Nombre, Edad, CorreoElectronico)
+VALUES ('María', 25, 'maria@example.com');
+
+INSERT INTO MiTabla (Nombre, Edad, CorreoElectronico)
+VALUES ('Carlos', 40, 'carlos@example.com');
